510ab1c5ba73fbf1570b6cdae52e53f967fcd46e
pu0238 <michal.lustyk>
2024-04-13 15:19:49 +0200

    added build scripts

--- src/warsztaty_rust_backend/src/lib.rs
+++ src/warsztaty_rust_backend/src/lib.rs
@@ -1,4 +1,20 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static WPISY: RefCell<Vec<String>> = RefCell::default();
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+#[ic_cdk::query]
+fn dodaj_wpis(wpis: String) {
+    WPISY.with(|wpisy| {
+        let mut mutable_wpisy = wpisy.borrow_mut();
+        mutable_wpisy.push(wpis);
+    });
+}
+
+ic_cdk::export_candid!();
