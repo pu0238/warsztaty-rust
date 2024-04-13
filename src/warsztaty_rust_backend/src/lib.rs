84ea6b14bfefbbd488bedef19f3b9e3920313e5b
pu0238 <michal.lustyk>
2024-04-13 15:35:43 +0200

    added pobierz_wpisy

--- src/warsztaty_rust_backend/src/lib.rs
+++ src/warsztaty_rust_backend/src/lib.rs
@@ -9,12 +9,17 @@ fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
 
-#[ic_cdk::query]
+#[ic_cdk::update]
 fn dodaj_wpis(wpis: String) {
     WPISY.with(|wpisy| {
         let mut mutable_wpisy = wpisy.borrow_mut();
         mutable_wpisy.push(wpis);
     });
+}
+
+#[ic_cdk::query]
+fn pobierz_wpisy() -> Vec<String>{
+    WPISY.with(|wpisy| wpisy.borrow().clone())
 }
 
 ic_cdk::export_candid!();
